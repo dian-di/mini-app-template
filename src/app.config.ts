4f679882e7d6962e3553b344446314100a859c50
yang <1203588983@qq.com>
2025-05-10 21:30:22 +0800

    add tabbar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,27 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/mine/index',
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: '#000000',
+    selectedColor: '#000000',
+    backgroundColor: '#fff',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+      },
+      {
+        pagePath: 'pages/mine/index',
+        text: '我的',
+      },
+    ],
+  },
 })
---
+++ src/pages/mine/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+
+export default function Index () {
+  useLoad(() => {
+    console.log('Page loaded.')
+  })
+
+  return (
+    <View className='text-blue-500'>
+      <Text>Mine</Text>
+    </View>
+  )
+}
